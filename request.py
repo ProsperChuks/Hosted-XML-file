7f84fa3720830b49caa657abe634eae088a5774b
ProsperChuks <prosperc40@gmail.com>
2020-02-14 03:26:23 +0100

    adding comments

--- request.py
+++ request.py
@@ -1,14 +1,18 @@
 import requests
 import xml.etree.ElementTree as et
 
+# Getting the URL
 xmlurl = requests.get("https://www.hindustantimes.com/rss/topnews/rssfeed.xml")
+# specifying the document type
 print(xmlurl.headers['content-type'])
 
 data = xmlurl.text
 
+# writing the document to an XML file
 with open("scrapedXML.xml", "w", newline='', encoding='utf-8') as scrapeddata:
     scrapeddata.write(data)
 
+# parsing the XML file
 link = et.parse("scrapedXML.xml")
 root = link.getroot()
 
